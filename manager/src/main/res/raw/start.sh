fd9d784db27f0fa6375995c930c4d8a9d0a25547
Rikka <rikka@xing.moe>
2017-07-14 01:58:40 +0800

    fix(starter): update sh

--- manager/src/main/res/raw/start.sh
+++ manager/src/main/res/raw/start.sh
@@ -1,15 +1,19 @@
 #!/system/bin/sh
 
 STARTER_PATH=%%%STARTER_PATH%%%
+STARTER_PARAM="%%%STARTER_PARAM%%%"
 
 echo "info: start.sh begin"
 
 if [ -f "$STARTER_PATH" ]; then
     rm /data/local/tmp/shizuku_starter 2> /dev/null
     cp "$STARTER_PATH" /data/local/tmp/shizuku_starter
-    chmod +x /data/local/tmp/shizuku_starter
+    chmod 755 /data/local/tmp/shizuku_starter
+    #if [ $USER -eq 0 ]; then
+    #    chown shell:shell /data/local/tmp/shizuku_starter
+    #fi
     export PATH=/data/local/tmp:/system/bin:$PATH
-    shizuku_starter
+    shizuku_starter $STARTER_PARAM
     result=$?
     if [ $result -ne 0 ]; then
         echo "shizuku_starter exit with non-zero value $result"
