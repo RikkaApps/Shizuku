5ed7ebbb47a2f56b5180ed249ba9edd38a71ee10
RikkaW <rikka@shizuku.moe>
2017-10-23 16:05:29 +0800

    fix BroadcastReceiver components are not allowed to bind to services

--- manager/src/main/java/moe/shizuku/manager/receiver/BootCompleteReceiver.java
+++ manager/src/main/java/moe/shizuku/manager/receiver/BootCompleteReceiver.java
@@ -3,16 +3,12 @@ package moe.shizuku.manager.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
 import android.util.Log;
 
 import moe.shizuku.manager.Constants;
-import moe.shizuku.manager.ServerLauncher;
 import moe.shizuku.manager.ShizukuManagerSettings;
 import moe.shizuku.manager.ShizukuManagerSettings.LaunchMethod;
-import moe.shizuku.manager.ShizukuManagerSettings.RootLaunchMethod;
-import moe.shizuku.manager.service.ShellService;
-import moe.shizuku.manager.utils.BindServiceHelper;
+import moe.shizuku.manager.service.BootCompleteService;
 
 /**
  * Created by Rikka on 2017/5/24.
@@ -29,36 +25,7 @@ public class BootCompleteReceiver extends BroadcastReceiver {
         if (ShizukuManagerSettings.getLastLaunchMode() == LaunchMethod.ROOT) {
             Log.i(Constants.TAG, "start on boot");
 
-            final BindServiceHelper helper = new BindServiceHelper(context, ShellService.class);
-            helper.bind(new BindServiceHelper.OnServiceConnectedListener() {
-                @Override
-                public void onServiceConnected(IBinder binder) {
-                    ShellService.ShellServiceBinder service = (ShellService.ShellServiceBinder) binder;
-
-                    ShellService.Listener listener = new ShellService.Listener() {
-                        @Override
-                        public void onFailed() {
-                            helper.unbind();
-                        }
-
-                        @Override
-                        public void onCommandResult(int commandCode, int exitCode) {
-                            helper.unbind();
-                        }
-
-                        @Override
-                        public void onLine(String line) {
-
-                        }
-                    };
-
-                    if (ShizukuManagerSettings.getRootLaunchMethod() == RootLaunchMethod.ALTERNATIVE) {
-                        service.run(ServerLauncher.COMMAND_ROOT_OLD, 0, listener);
-                    } else {
-                        service.run(ServerLauncher.COMMAND_ROOT, 0, listener);
-                    }
-                }
-            });
+            context.startService(new Intent(context, BootCompleteService.class));
         }
     }
 }
