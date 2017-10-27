af937239cb05c37939ff253f5bdd152c901395a4
Rikka <rikka@shizuku.moe>
2017-10-27 16:57:42 +0800

    manager: app list sort by name

--- manager/src/main/java/moe/shizuku/manager/ManageAppsActivity.java
+++ manager/src/main/java/moe/shizuku/manager/ManageAppsActivity.java
@@ -1,13 +1,19 @@
 package moe.shizuku.manager;
 
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import moe.shizuku.manager.adapter.AppAdapter;
 import moe.shizuku.manager.authorization.AuthorizationManager;
+import moe.shizuku.manager.utils.AppNameComparator;
 import moe.shizuku.support.recyclerview.RecyclerViewHelper;
 
 public class ManageAppsActivity extends BaseActivity {
@@ -25,17 +31,21 @@ public class ManageAppsActivity extends BaseActivity {
         }
 
         AppAdapter adapter = new AppAdapter();
+
+        List<PackageInfo> list = new ArrayList<>();
         for (String packageName : AuthorizationManager.getPackages(this)) {
             if (BuildConfig.APPLICATION_ID.equals(packageName)) {
                 continue;
             }
 
             try {
-                adapter.getItems().add(getPackageManager().getPackageInfo(packageName, 0));
+                list.add(getPackageManager().getPackageInfo(packageName, 0));
             } catch (PackageManager.NameNotFoundException ignored) {
             }
-            break;
         }
+
+        Collections.sort(list, new AppNameComparator(this).getPackageInfoComparator());
+        adapter.getItems().addAll(list);
 
         RecyclerView recyclerView = findViewById(android.R.id.list);
         recyclerView.setAdapter(adapter);
