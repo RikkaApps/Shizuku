4ed455623695ace75662651e1f21a7a672cc23ba
RikkaW <rikka@shizuku.moe>
2019-07-21 09:24:07 +0800

    add copy adb command back

--- manager/src/main/java/moe/shizuku/manager/viewholder/StartAdbViewHolder.java
+++ manager/src/main/java/moe/shizuku/manager/viewholder/StartAdbViewHolder.java
@@ -1,12 +1,17 @@
 package moe.shizuku.manager.viewholder;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
+import android.widget.Toast;
 
 import moe.shizuku.manager.Helps;
 import moe.shizuku.manager.R;
+import moe.shizuku.manager.ServerLauncher;
 import moe.shizuku.manager.utils.CustomTabsHelper;
 import moe.shizuku.manager.utils.MultiLocaleEntity;
 import moe.shizuku.support.recyclerview.BaseViewHolder;
+import moe.shizuku.support.utils.ClipboardUtils;
 
 public class StartAdbViewHolder extends BaseViewHolder<Object> {
 
@@ -17,11 +22,20 @@ public class StartAdbViewHolder extends BaseViewHolder<Object> {
 
         itemView.findViewById(android.R.id.button1).setOnClickListener(v -> CustomTabsHelper.launchUrlOrCopy(v.getContext(), Helps.ADB.get()));
 
-        /*itemView.findViewById(android.R.id.button2).setOnClickListener(v -> {
+        itemView.findViewById(android.R.id.button2).setOnClickListener(v -> {
             Context context = v.getContext();
             if (ClipboardUtils.put(context, ServerLauncher.COMMAND_ADB)) {
                 Toast.makeText(context, context.getString(R.string.copied_to_clipboard, ServerLauncher.COMMAND_ADB), Toast.LENGTH_SHORT).show();
             }
-        });*/
+        });
+
+        itemView.findViewById(android.R.id.button3).setOnClickListener(v -> {
+            Context context = v.getContext();
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, ServerLauncher.COMMAND_ADB);
+            intent = Intent.createChooser(intent, context.getString(R.string.send_command));
+            context.startActivity(intent);
+        });
     }
 }
