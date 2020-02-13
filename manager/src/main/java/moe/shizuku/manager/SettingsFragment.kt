70613befb58a25fd6dcc1a578ff9ccf8d9ba5c10
RikkaW <rikka@shizuku.moe>
2020-02-13 14:31:05 +0800

    changes for ui

--- manager/src/main/java/moe/shizuku/manager/SettingsFragment.kt
+++ manager/src/main/java/moe/shizuku/manager/SettingsFragment.kt
@@ -9,7 +9,7 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.RecyclerView
-import moe.shizuku.manager.app.ThemeHelper
+import moe.shizuku.manager.app.ThemeHelper.KEY_BLACK_NIGHT_THEME
 import moe.shizuku.manager.viewmodel.AppsViewModel
 import moe.shizuku.manager.viewmodel.SharedViewModelProviders
 import moe.shizuku.manager.viewmodel.Status
@@ -21,25 +21,24 @@ import rikka.material.app.LocaleDelegate
 import rikka.material.widget.*
 import rikka.recyclerview.RecyclerViewHelper
 import java.util.*
+import moe.shizuku.manager.ShizukuManagerSettings.KEEP_SU_CONTEXT as KEY_KEEP_SU_CONTEXT
+import moe.shizuku.manager.ShizukuManagerSettings.LANGUAGE as KEY_LANGUAGE
+import moe.shizuku.manager.ShizukuManagerSettings.NIGHT_MODE as KEY_NIGHT_MODE
+import moe.shizuku.manager.ShizukuManagerSettings.NO_V2 as KEY_NO_V2
 
 class SettingsFragment : PreferenceFragment() {
 
     companion object {
-
-        const val KEY_LANGUAGE = ShizukuManagerSettings.LANGUAGE
-        const val KEY_NIGHT_MODE = ShizukuManagerSettings.NIGHT_MODE
-        const val KEY_BLACK_NIGHT_THEME = ThemeHelper.KEY_BLACK_NIGHT_THEME
-        const val KEY_NO_V2 = ShizukuManagerSettings.NO_V2
-
         init {
             SimpleMenuPreference.setLightFixEnabled(true)
         }
     }
 
-    private var languagePreference: ListPreference? = null
-    private var nightModePreference: Preference? = null
-    private var blackNightThemePreference: SwitchPreference? = null
-    private var noV2Preference: SwitchPreference? = null
+    private lateinit var languagePreference: ListPreference
+    private lateinit var nightModePreference: Preference
+    private lateinit var blackNightThemePreference: SwitchPreference
+    private lateinit var noV2Preference: SwitchPreference
+    private lateinit var keepSuContextPreference: SwitchPreference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         preferenceManager.setStorageDeviceProtected()
@@ -51,6 +50,9 @@ class SettingsFragment : PreferenceFragment() {
         nightModePreference = findPreference(KEY_NIGHT_MODE)
         blackNightThemePreference = findPreference(KEY_BLACK_NIGHT_THEME) as SwitchPreference
         noV2Preference = findPreference(KEY_NO_V2) as SwitchPreference
+        keepSuContextPreference = findPreference(KEY_KEEP_SU_CONTEXT) as SwitchPreference
+
+        keepSuContextPreference.isVisible = false
 
         val viewModel = SharedViewModelProviders.of(this).get(AppsViewModel::class.java)
         viewModel.packages.observe(this) {
@@ -62,7 +64,7 @@ class SettingsFragment : PreferenceFragment() {
             viewModel.load()
         }
 
-        languagePreference!!.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _: Preference?, newValue: Any ->
+        languagePreference.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _: Preference?, newValue: Any ->
             if (newValue is String) {
                 val locale: Locale = if ("SYSTEM" == newValue) {
                     LocaleDelegate.systemLocale
@@ -75,35 +77,35 @@ class SettingsFragment : PreferenceFragment() {
             true
         }
 
-        val tag = languagePreference!!.value
-        val index = listOf(*languagePreference!!.entryValues).indexOf(tag)
+        val tag = languagePreference.value
+        val index = listOf(*languagePreference.entryValues).indexOf(tag)
         val localeName: MutableList<String> = ArrayList()
         val localeNameUser: MutableList<String> = ArrayList()
         val userLocale = ShizukuManagerSettings.getLocale()
-        for (i in 1 until languagePreference!!.entries.size) {
-            val locale = Locale.forLanguageTag(languagePreference!!.entries[i].toString())
+        for (i in 1 until languagePreference.entries.size) {
+            val locale = Locale.forLanguageTag(languagePreference.entries[i].toString())
             localeName.add(if (!TextUtils.isEmpty(locale.script)) locale.getDisplayScript(locale) else locale.getDisplayName(locale))
             localeNameUser.add(if (!TextUtils.isEmpty(locale.script)) locale.getDisplayScript(userLocale) else locale.getDisplayName(userLocale))
         }
 
-        for (i in 1 until languagePreference!!.entries.size) {
+        for (i in 1 until languagePreference.entries.size) {
             if (index != i) {
-                languagePreference!!.entries[i] = HtmlCompat.fromHtml(String.format("%s - %s",
+                languagePreference.entries[i] = HtmlCompat.fromHtml(String.format("%s - %s",
                         localeName[i - 1],
                         localeNameUser[i - 1]
                 ))
             } else {
-                languagePreference!!.entries[i] = localeNameUser[i - 1]
+                languagePreference.entries[i] = localeNameUser[i - 1]
             }
         }
 
         if (TextUtils.isEmpty(tag) || "SYSTEM" == tag) {
-            languagePreference!!.summary = getString(R.string.follow_system)
+            languagePreference.summary = getString(R.string.follow_system)
         } else if (index != -1) {
             val name = localeNameUser[index - 1]
-            languagePreference!!.summary = name
+            languagePreference.summary = name
         }
-        nightModePreference!!.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _: Preference?, value: Any? ->
+        nightModePreference.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _: Preference?, value: Any? ->
             if (value is Int) {
                 if (ShizukuManagerSettings.getNightMode() != value) {
                     DayNightDelegate.setDefaultNightMode(value)
@@ -112,7 +114,7 @@ class SettingsFragment : PreferenceFragment() {
             }
             true
         }
-        blackNightThemePreference!!.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _: Preference?, _: Any? ->
+        blackNightThemePreference.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _: Preference?, _: Any? ->
             if (ResourceUtils.isNightMode(requireContext().resources.configuration)) activity?.recreate()
             true
         }
@@ -145,12 +147,14 @@ class SettingsFragment : PreferenceFragment() {
 
     private fun updateData(data: List<PackageInfo>?) {
         var count = 0
-        for (pi in data!!) {
-            val ai = pi.applicationInfo
-            if (ai.metaData == null || !ai.metaData.getBoolean("moe.shizuku.client.V3_SUPPORT")) {
-                count++
+        if (data != null) {
+            for (pi in data) {
+                val ai = pi.applicationInfo
+                if (ai.metaData == null || !ai.metaData.getBoolean("moe.shizuku.client.V3_SUPPORT")) {
+                    count++
+                }
             }
         }
-        noV2Preference!!.summary = requireContext().resources.getQuantityString(R.plurals.start_legacy_service_summary, count, count)
+        noV2Preference.summary = requireContext().resources.getQuantityString(R.plurals.start_legacy_service_summary, count, count)
     }
 }
