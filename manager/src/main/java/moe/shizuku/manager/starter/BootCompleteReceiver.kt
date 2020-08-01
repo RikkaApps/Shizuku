984c5761cd9d1472a477014d4baf2dcc04d944bc
RikkaW <rikka@xing.moe>
2020-08-01 13:31:08 +0800

    write script files (to reset file permissions) on boot

---
+++ manager/src/main/java/moe/shizuku/manager/starter/BootCompleteReceiver.kt
@@ -0,0 +1,33 @@
+package moe.shizuku.manager.starter
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Process
+import android.util.Log
+import androidx.core.content.ContextCompat
+import moe.shizuku.api.ShizukuService
+import moe.shizuku.manager.AppConstants
+import moe.shizuku.manager.ShizukuSettings
+import moe.shizuku.manager.ShizukuSettings.LaunchMethod
+
+class BootCompleteReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Intent.ACTION_LOCKED_BOOT_COMPLETED != intent.action
+                && Intent.ACTION_BOOT_COMPLETED != intent.action) {
+            return
+        }
+        if (Process.myUid() / 100000 > 0) return
+
+        Starter.writeFiles(context)
+
+        if (ShizukuSettings.getLastLaunchMode() == LaunchMethod.ROOT) {
+            Log.i(AppConstants.TAG, "start on boot, action=" + intent.action)
+            if (ShizukuService.pingBinder()) {
+                Log.i(AppConstants.TAG, "service is running")
+                return
+            }
+            ContextCompat.startForegroundService(context, Intent(context, BootCompleteService::class.java))
+        }
+    }
+}
