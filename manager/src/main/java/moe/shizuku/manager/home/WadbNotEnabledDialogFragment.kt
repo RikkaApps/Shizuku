a58c31833cec08e9158dcb07a594178a0adeb540
RikkaW <rikka@xing.moe>
2020-10-12 16:22:18 +0800

    allow to use wadb without prop check
    maybe useful like #81 (weird TV device with wadb enabled but prop not set?)

--- manager/src/main/java/moe/shizuku/manager/home/WadbNotEnabledDialogFragment.kt
+++ manager/src/main/java/moe/shizuku/manager/home/WadbNotEnabledDialogFragment.kt
@@ -1,11 +1,14 @@
 package moe.shizuku.manager.home
 
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import moe.shizuku.manager.R
+import moe.shizuku.manager.starter.StarterActivity
+import java.net.InetAddress
 
 class WadbNotEnabledDialogFragment :DialogFragment() {
 
@@ -14,6 +17,15 @@ class WadbNotEnabledDialogFragment :DialogFragment() {
         return AlertDialog.Builder(context)
                 .setMessage(R.string.dialog_wireless_adb_not_enabled)
                 .setPositiveButton(android.R.string.ok, null)
+                .setNeutralButton(context.getString(R.string.dialog_adb_try, 5555)) { _, _ ->
+                    val host = InetAddress.getLoopbackAddress().hostName
+                    val intent = Intent(context, StarterActivity::class.java).apply {
+                        putExtra(StarterActivity.EXTRA_IS_ROOT, false)
+                        putExtra(StarterActivity.EXTRA_HOST, host)
+                        putExtra(StarterActivity.EXTRA_PORT, 5555)
+                    }
+                    context.startActivity(intent)
+                }
                 .create()
     }
 
