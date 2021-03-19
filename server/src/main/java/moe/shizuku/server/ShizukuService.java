25833b1e1db9eb49c6e768ec6f570213e863adc8
RikkaW <rikka@shizuku.moe>
2021-03-19 14:40:02 +0800

    Fix typo

--- server/src/main/java/moe/shizuku/server/ShizukuService.java
+++ server/src/main/java/moe/shizuku/server/ShizukuService.java
@@ -714,7 +714,7 @@ public class ShizukuService extends IShizukuService.Stub {
 
         List<ClientRecord> records = clientManager.findClients(requestUid);
         if (records.isEmpty()) {
-            LOGGER.w("dispatchPermissionConfirmationResult: no client for uid % was found", requestUid, requestPid);
+            LOGGER.w("dispatchPermissionConfirmationResult: no client for uid %d was found", requestUid);
         } else {
             for (ClientRecord record : records) {
                 record.allowed = allowed;
