4a608209508864c75b47a566b32ed6d6aaa5174d
ryuunoakaihitomi <22442525+ryuunoakaihitomi@users.noreply.github.com>
2021-10-25 13:08:50 +0800

    Check notification permission

--- manager/src/main/java/moe/shizuku/manager/home/AdbPairTutorialDialogFragment.kt
+++ manager/src/main/java/moe/shizuku/manager/home/AdbPairTutorialDialogFragment.kt
@@ -8,22 +8,27 @@ import android.os.Bundle
 import android.provider.Settings
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import moe.shizuku.manager.R
 import moe.shizuku.manager.ktx.toHtml
+import rikka.core.util.IntentUtils
 import rikka.html.text.HtmlCompat
+import rikka.shizuku.ShizukuProvider
 
 @RequiresApi(VERSION_CODES.R)
 class AdbPairTutorialDialogFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val context = requireContext()
+        val enabled = NotificationManagerCompat.from(context).areNotificationsEnabled()
 
         val builder = AlertDialog.Builder(context).apply {
             setTitle(R.string.adb_pairing_tutorial_title)
             setMessage(
-                (getString(R.string.adb_pairing_tutorial_content_start_service) + "<p>" +
+                ((if (!enabled) "<b>" + getString(R.string.adb_pairing_tutorial_content_enable_notification) + "</b><p>" else "") +
+                        getString(R.string.adb_pairing_tutorial_content_start_service) + "<p>" +
                         getString(R.string.adb_pairing_tutorial_content_input_pairing_code) + "<p>" +
                         "<small>" + getString(R.string.adb_pairing_tutorial_content_bad_system) + "</small>").toHtml(
                     HtmlCompat.FROM_HTML_OPTION_TRIM_WHITESPACE
@@ -31,6 +36,9 @@ class AdbPairTutorialDialogFragment : DialogFragment() {
             )
             setPositiveButton(android.R.string.ok, null)
             setNeutralButton(R.string.development_settings, null)
+            if (!enabled) {
+                setNegativeButton(R.string.notification_settings, null)
+            }
         }
         val dialog = builder.create()
         dialog.setCanceledOnTouchOutside(false)
@@ -46,6 +54,11 @@ class AdbPairTutorialDialogFragment : DialogFragment() {
                 it.context.startActivity(intent)
             } catch (e: ActivityNotFoundException) {
             }
+        }
+        dialog.getButton(AlertDialog.BUTTON_NEGATIVE)?.setOnClickListener {
+            val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, ShizukuProvider.MANAGER_APPLICATION_ID)
+            IntentUtils.startActivity(context, intent, null)
         }
     }
 
