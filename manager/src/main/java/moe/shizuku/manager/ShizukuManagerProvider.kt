95c39efa299f16002b42d7e95781934b23bc2293
RikkaW <rikka@shizuku.moe>
2021-10-27 20:59:36 +0800

    Update submodule

--- manager/src/main/java/moe/shizuku/manager/ShizukuManagerProvider.kt
+++ manager/src/main/java/moe/shizuku/manager/ShizukuManagerProvider.kt
@@ -14,6 +14,11 @@ class ShizukuManagerProvider : ShizukuProvider() {
         private const val METHOD_SEND_USER_SERVICE = "sendUserService"
     }
 
+    override fun onCreate(): Boolean {
+        disableAutomaticSuiInitialization()
+        return super.onCreate()
+    }
+
     override fun call(method: String, arg: String?, extras: Bundle?): Bundle? {
         if (extras == null) return null
 
