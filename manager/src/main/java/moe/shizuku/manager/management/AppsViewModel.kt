1d43a05d2371697f51da6533cec8406878dab014
vvb2060 <vvb2060@gmail.com>
2025-05-24 10:55:08 +0000

    Fix granted count

--- manager/src/main/java/moe/shizuku/manager/management/AppsViewModel.kt
+++ manager/src/main/java/moe/shizuku/manager/management/AppsViewModel.kt
@@ -32,7 +32,7 @@ class AppsViewModel(context: Context) : ViewModel() {
     private val _grantedCount = MutableLiveData<Resource<Int>>()
     val grantedCount = _grantedCount as LiveData<Resource<Int>>
 
-    fun load() {
+    fun load(onlyCount: Boolean = false) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 val list: MutableList<PackageInfo> = ArrayList()
@@ -41,30 +41,8 @@ class AppsViewModel(context: Context) : ViewModel() {
                     list.add(pi)
                     if (AuthorizationManager.granted(pi.packageName, pi.applicationInfo!!.uid)) count++
                 }
-                _packages.postValue(Resource.success(list))
+                if (!onlyCount) _packages.postValue(Resource.success(list))
                 _grantedCount.postValue(Resource.success(count))
-            } catch (e: CancellationException) {
-
-            } catch (e: Throwable) {
-                _packages.postValue(Resource.error(e, null))
-                _grantedCount.postValue(Resource.error(e, 0))
-            }
-        }
-    }
-
-    fun loadCount() {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val list: MutableList<PackageInfo> = ArrayList()
-                val packages: MutableList<String> = ArrayList()
-                for (pi in AuthorizationManager.getPackages()) {
-                    list.add(pi)
-                    if (AuthorizationManager.granted(
-                            pi.packageName,
-                            pi.applicationInfo!!.uid
-                        )
-                    ) packages.add(pi.packageName)
-                }
             } catch (e: CancellationException) {
 
             } catch (e: Throwable) {
